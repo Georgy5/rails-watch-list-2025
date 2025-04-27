387ad52df0794de858668992ea0167c1f197e334
Karl Keller <kgk86@hotmail.com>
2025-04-27 17:36:57 +0200

    Add #destroy action to BookmarksController
    Passes RSpec test "deletes a bookmark"

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -12,6 +12,12 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to lists_path(@bookmark.list)
+  end
+
   private
 
     def bookmark_params
